1df9e305456ddbb67c7691075666886405de9c68
Azizbek Samatov <azizbeksamatov98@gmail.com>
2021-12-14 16:22:32 +0500

    feat: adding basic layout in app.js for main logic of application

--- js/app.js
+++ js/app.js
@@ -9,5 +9,29 @@ todoButton.addEventListener("click", addTodo);
 // Functions
 function addTodo(event) {
   event.preventDefault(); // preventing from submitting form
-  console.log("hello from todo app");
+
+  // creating todo DIV here
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // creating li here
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "todo example text";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo); // creating li element inside div
+
+  // completing task button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // removing task button
+  const removeButton = document.createElement("button");
+  removeButton.innerHTML = '<i class="fas fa-trash"></i>';
+  removeButton.classList.add("remove-btn");
+  todoDiv.appendChild(removeButton);
+
+  // appending to todo-list
+  todoList.appendChild(todoDiv);
 }
